3d3dc217852be227ff00f8c6a96ae149c54245e2
William Steinke de Mello <62147577+lion772@users.noreply.github.com>
2021-06-19 01:08:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 'use strict';
 
+let randomNumber = Math.floor(Math.random() * 20);
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-
   // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
-    displayMessage('⛔️ No number!');
   }
+
+  if (guess === randomNumber) {
+    document.querySelector('.message').textContent =
+      '🎉 You found the correct number!';
+    document.body.style.backgroundColor = 'green';
+  } else {
+    if (guess >= randomNumber - 3 && guess < randomNumber) {
+      document.querySelector('.message').textContent = 'Almost there...';
+    } else if (guess <= randomNumber + 3 && guess > randomNumber) {
+      document.querySelector('.message').textContent = 'Almost there...';
+    } else if (guess < randomNumber) {
+      document.querySelector('.message').textContent = 'Too low';
+    } else {
+      document.querySelector('.message').textContent = 'Too high';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  randomNumber = Math.floor(Math.random() * 6);
 });
