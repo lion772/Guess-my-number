43b87b21109ed386dec31ffc1d10ed5485490fdb
William Steinke de Mello <62147577+lion772@users.noreply.github.com>
2021-06-19 00:30:25 +0200

    Adding JS file

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+  }
+});
