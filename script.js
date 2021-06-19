e5e082a3a47d1dba597457eecf6da34ef3f864f0
William Steinke de Mello <62147577+lion772@users.noreply.github.com>
2021-06-19 16:19:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,47 +7,51 @@ document.querySelector('.check').addEventListener('click', function () {
   //The value returned from the input is a string that must be converted to a number.
   //If there's no value in it, it returns a falsy value '0'.
   const guess = Number(document.querySelector('.guess').value);
-  let score = document.querySelector('.score').textContent;
-
-  //#Correct: when it is 0, it doesn't work.' ✔
+  let score = Number(document.querySelector('.score').textContent);
 
   // When there is no input
-  if (guess <= 0) {
-    document.querySelector('.message').textContent =
-      '⛔️ Try to guess a number between 1 and 20';
-  } else {
-    // When there is input
-    if (guess === randomNumber) {
+  if (score > 1) {
+    if (guess <= 0) {
       document.querySelector('.message').textContent =
-        '🎉 You found the correct number!';
-      document.body.style.backgroundColor = 'green';
-      document.querySelector('.interrogation').textContent = String(guess);
-      document.querySelector('.highscore').textContent = score;
+        '⛔️ Try to guess a number between 1 and 20';
     } else {
-      if (guess >= randomNumber - 3 && guess < randomNumber) {
-        document.querySelector('.message').textContent = 'Almost there...';
-        score -= 1;
-      } else if (guess <= randomNumber + 3 && guess > randomNumber) {
-        document.querySelector('.message').textContent = 'Almost there...';
-        score -= 1;
-      } else if (guess < randomNumber) {
-        document.querySelector('.message').textContent = 'Too low';
-        score -= 1;
+      // When there is input
+      if (guess === randomNumber) {
+        document.querySelector('.message').textContent =
+          '🎉 You found the correct number!';
+        document.body.style.backgroundColor = '#60B347';
+        document.querySelector('.interrogation').textContent = String(guess);
+        document.querySelector('.highscore').textContent = score;
       } else {
-        document.querySelector('.message').textContent = 'Too high';
-        score -= 1;
+        if (
+          (guess >= randomNumber - 3 && guess < randomNumber) ||
+          (guess <= randomNumber + 3 && guess > randomNumber)
+        ) {
+          document.querySelector('.message').textContent = 'Almost there...';
+          score -= 1;
+        } else if (guess < randomNumber) {
+          document.querySelector('.message').textContent = 'Too low';
+          score -= 1;
+        } else {
+          document.querySelector('.message').textContent = 'Too high';
+          score -= 1;
+        }
+        document.querySelector('.score').textContent = score;
       }
-      document.querySelector('.score').textContent = score;
     }
+  } else {
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('.message').textContent = 'You lost the game... 😢';
+    document.body.style.backgroundColor = 'red';
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  randomNumber = Math.floor(Math.random() * 6);
+  randomNumber = Math.ceil(Math.random() * 20);
   document.body.style.backgroundColor = '#222';
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.interrogation').textContent = '?';
   document.querySelector('.guess').value = '';
-  document.querySelector('.score').textContent = 20;
+  document.querySelector('.score').textContent = 3;
   document.querySelector('.highscore').textContent = 0;
 });
